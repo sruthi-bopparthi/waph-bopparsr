fd27b37e684e24eecda859bb86dd73c18ecfcc1a
SruthiAelay <bopparsr@mail.uc.edu>
2024-01-23 18:43:32 -0500

    Added files

---
+++ labs/lab1/helloworld.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("Content-Type: text/plain; charset=utf-8\n\n");
+	printf("Hello World CGI! From Sruthi Bopparthi, WAPH\n\n");
+	return 0;
+}
---
+++ labs/lab1/index.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("Content-Type: text/html; charset=utf-8\n\n");
+	printf("<html>\n");
+	printf("<head><title>WAPH</title></head>");
+	printf("<body>\n");
+	printf("<h1>Lab1 - Sruthi Bopparthi</h1>\n");
+	printf("<p>I love to so study CGI</p>");
+	printf("</body>");
+	printf("</html>");
+	return 0;
+}
