590cf6683845e9ef16879df60b03bee71946c169
SruthiAelay <bopparsr@mail.uc.edu>
2024-02-08 20:25:07 -0500

    updated

--- labs/lab1/echo.php
+++ labs/lab1/echo.php
@@ -1,3 +1,8 @@
 <?php
-  echo $_REQUEST["data"];
+  if(empty($_REQUEST["data"]))
+  {
+    exit("please enter the input field data");
+  }
+  $input=htmlentities($_REQUEST["data"]);
+  echo ("The input from the request is <strong>". $input."</strong>.<br>");
 ?>
